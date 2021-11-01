fc1bbe603a277fd11411f03e8f7e2675819e5e63
MariaMarousopoulou <marousopoulou@gmail.com>
2021-11-01 21:00:22 +0200

    Preprocess Completed

--- dataPreprocess.py
+++ dataPreprocess.py
@@ -6,8 +6,6 @@ Created on Sun Oct 31 10:38:28 2021
 """
 
 import pandas as pd
-from nltk.corpus import stopwords
-from sklearn.feature_extraction import text
 
 sidewayEmoticons = [':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}',
                     ':^)', ':-D', ':D', '8-D', '8D', 'x-D', 'xD', 'X-D', 'XD', '=-D', '=D',
@@ -17,31 +15,67 @@ sidewayEmoticons = [':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)
                     ':-[', ':-<', '=\\', '=/', '>:(', ':(', '>.<', ":'-(", ":'(", ':\\', ':-c',
                     ':c', ':{', '>:\\', ';(']
 
+punctuationSymbols = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
 # Load data
 txtData = pd.read_csv('dataset_ubicomp2013_tips.txt', sep='\t', encoding='ISO-8859-1', header=None)
 txtData.columns = ['storeId', 'userId', 'comment']
 
-# Preprocess data
+# Missing values
+txtData.isnull().sum()  # No missing valuese
+
 txtData = txtData.drop('userId', 1)  # drop user
 txtData['comment'] = txtData['comment'].str.lower()  # convert to lowercase
 
-# NLTK Stopwords
-txtDataNltk = txtData.copy()
-nltkStopwords = stopwords.words('english')
-txtDataNltk['comment'] = txtDataNltk['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
-                                                                          nltkStopwords]))
-txtDataNltkNoEmoticons = txtDataNltk.copy()
-txtDataNltkNoEmoticons['comment'] = txtDataNltkNoEmoticons['comment'].apply(
-    lambda x: ' '.join([word for word in x.split()
-                        if word not in sidewayEmoticons]))
+# Remove extra whitespaces
+txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join(x.split()))
 
-# SKLearn Stopwords
-txtDataSklearn = txtData.copy()
-sklearnStopwords = text.ENGLISH_STOP_WORDS
-txtDataSklearn['comment'] = txtDataSklearn['comment'].apply(
-    lambda x: ' '.join([word for word in x.split()
-                        if word not in sklearnStopwords]))
-txtDataSklearnNoEmoticons = txtDataSklearn.copy()
-txtDataSklearnNoEmoticons['comment'] = txtDataSklearnNoEmoticons['comment'].apply(
-    lambda x: ' '.join([word for word in x.split()
-                        if word not in sidewayEmoticons]))
+# Remove AM/PM
+txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
+                                                                  ('(?:\d{2}|\d{1})(?:AM|PM|am|pm)')]))
+
+# Remove url
+txtData['comment'] = txtData['comment'].str.replace(r'http\S+', '')
+
+# Remove emojis
+txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
+                                                                  sidewayEmoticons]))
+
+# Remove symbols
+txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
+                                                                  punctuationSymbols]))
+
+# Save to csv
+txtData.to_csv('preProcessedData.csv', sep='\t', encoding='utf-8', index=False, header=True)
+# Read from csv
+# txtData = pd.read_csv('preProcessedData.csv', sep='\t', encoding='utf-8')
+
+
+# # NLTK Stopwords
+# txtDataNltk = txtData.copy()
+# nltkStopwords = stopwords.words('english')
+# txtDataNltk['comment'] = txtDataNltk['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
+#                                                                           nltkStopwords]))
+
+# txtDataNltkNoSymbols = txtDataNltk.copy()  # Remove symbols
+# txtDataNltkNoSymbols['comment'] = txtDataNltkNoSymbols['comment'].apply(
+#     lambda x: ' '.join([word for word in x.split()
+#                         if word not in sidewayEmoticons]))
+# txtDataNltkNoSymbols['comment'] = txtDataNltkNoSymbols['comment'].apply(
+#     lambda x: ' '.join([word for word in x.split()
+#                         if word not in punctuationSymbols]))
+
+# # SKLearn Stopwords
+# txtDataSklearn = txtData.copy()
+# sklearnStopwords = text.ENGLISH_STOP_WORDS
+# txtDataSklearn['comment'] = txtDataSklearn['comment'].apply(
+#     lambda x: ' '.join([word for word in x.split()
+#                         if word not in sklearnStopwords]))
+
+# txtDataSklearnNoSymbols = txtDataSklearn.copy()  # Remove symbols
+# txtDataSklearnNoSymbols['comment'] = txtDataSklearnNoSymbols['comment'].apply(
+#     lambda x: ' '.join([word for word in x.split()
+#                         if word not in sidewayEmoticons]))
+# txtDataSklearnNoSymbols['comment'] = txtDataSklearnNoSymbols['comment'].apply(
+#     lambda x: ' '.join([word for word in x.split()
+#                         if word not in punctuationSymbols]))
--- doc2Vec.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 28 20:40:28 2021
-
-@author: Maria_Marousopoulou
-"""
-
