7a8d49b9cb7355308507d811e6b76045143efe37
MariaMarousopoulou <marousopoulou@gmail.com>
2021-11-10 20:57:30 +0200

    Preprocess Completed

--- dataPreprocess.py
+++ dataPreprocess.py
@@ -8,7 +8,8 @@ Created on Sun Oct 31 10:38:28 2021
 import pandas as pd
 from autocorrect import spell
 from nltk import word_tokenize
-
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 sidewayEmoticons = [':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}',
                     ':^)', ':-D', ':D', '8-D', '8D', 'x-D', 'xD', 'X-D', 'XD', '=-D', '=D',
@@ -17,13 +18,6 @@ sidewayEmoticons = [':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)
                     '<3', ':L', ':-/', '>:/', ':S', '>:[', ':@', ':-(', ':[', ':-||', '=L', ':<',
                     ':-[', ':-<', '=\\', '=/', '>:(', ':(', '>.<', ":'-(", ":'(", ':\\', ':-c',
                     ':c', ':{', '>:\\', ';(']
-
-punctuationSymbols = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-
-# Strip unwanted characters/markup  (e.g. HTML tags, punctuation, numbers, etc.).
-# Break into tokens (keywords) on whitespace.
-# Stem tokens to “root” words -> computational  comput
-# Remove common stopwords (e.g. a, the, it, etc.).
 
 # Load data
 txtData = pd.read_csv('dataset_ubicomp2013_tips.txt', sep='\t', encoding='ISO-8859-1', header=None)
@@ -47,8 +41,7 @@ txtData['comment'] = txtData['comment'].str.replace(r'http\S+', '')
 txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
                                                                   sidewayEmoticons]))
 # Remove symbols
-txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
-                                                                  punctuationSymbols]))
+txtData['comment'] = txtData['comment'].str.replace(r'[^\w\s]+', '')
 
 # All to string
 txtData['comment'] = txtData['comment'].apply(str)
@@ -56,27 +49,20 @@ txtData['comment'] = txtData['comment'].apply(str)
 # Spelling corrections
 txtData['comment'] = [' '.join([spell(i) for i in x.split()]) for x in txtData['comment']]
 
+# Porter Stemmer
+nltkPorterStemmer = PorterStemmer()
+txtData['comment'].apply(lambda x: [nltkPorterStemmer.stem(y) for y in x])
+
+# NLTK Stopwords
+nltkStopwords = stopwords.words('english')
+txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
+                                                                  nltkStopwords]))
+
 # Tokenization
 txtData['comment'] = txtData['comment'].apply(lambda x: word_tokenize(x))
-
 
 # Save to csv
 txtData.to_csv('processedData.csv', sep='\t', encoding='utf-8', index=False, header=True)
 
-# Read from csv
+# Read from csv to check
 newTxtData = pd.read_csv('processedData.csv', sep='\t', encoding='utf-8')
-
-
-# # NLTK Stopwords
-# txtDataNltk = txtData.copy()
-# nltkStopwords = stopwords.words('english')
-# txtDataNltk['comment'] = txtDataNltk['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
-#                                                                           nltkStopwords]))
-
-# txtDataNltkNoSymbols = txtDataNltk.copy()  # Remove symbols
-# txtDataNltkNoSymbols['comment'] = txtDataNltkNoSymbols['comment'].apply(
-#     lambda x: ' '.join([word for word in x.split()
-#                         if word not in sidewayEmoticons]))
-# txtDataNltkNoSymbols['comment'] = txtDataNltkNoSymbols['comment'].apply(
-#     lambda x: ' '.join([word for word in x.split()
-#                         if word not in punctuationSymbols]))
