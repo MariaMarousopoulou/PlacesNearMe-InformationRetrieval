732bb08a85c99b0da8a93a5ff0c134b76f40f90d
MariaMarousopoulou <marousopoulou@gmail.com>
2021-11-10 19:39:31 +0200

    Wrap up

--- dataPreprocess.py
+++ dataPreprocess.py
@@ -6,6 +6,9 @@ Created on Sun Oct 31 10:38:28 2021
 """
 
 import pandas as pd
+from autocorrect import spell
+from nltk import word_tokenize
+
 
 sidewayEmoticons = [':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}',
                     ':^)', ':-D', ':D', '8-D', '8D', 'x-D', 'xD', 'X-D', 'XD', '=-D', '=D',
@@ -17,14 +20,18 @@ sidewayEmoticons = [':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)
 
 punctuationSymbols = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
+# Strip unwanted characters/markup  (e.g. HTML tags, punctuation, numbers, etc.).
+# Break into tokens (keywords) on whitespace.
+# Stem tokens to “root” words -> computational  comput
+# Remove common stopwords (e.g. a, the, it, etc.).
+
 # Load data
 txtData = pd.read_csv('dataset_ubicomp2013_tips.txt', sep='\t', encoding='ISO-8859-1', header=None)
-txtData.columns = ['storeId', 'userId', 'comment']
+txtData.columns = ['storeId', 'comment']
 
 # Missing values
 txtData.isnull().sum()  # No missing values
 
-txtData = txtData.drop('userId', 1)  # drop user
 txtData['comment'] = txtData['comment'].str.lower()  # convert to lowercase
 
 # Remove extra whitespaces
@@ -33,23 +40,31 @@ txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join(x.split()))
 # Remove AM/PM
 txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
                                                                   ('(?:\d{2}|\d{1})(?:AM|PM|am|pm)')]))
-
 # Remove url
 txtData['comment'] = txtData['comment'].str.replace(r'http\S+', '')
 
 # Remove emojis
 txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
                                                                   sidewayEmoticons]))
-
 # Remove symbols
 txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
                                                                   punctuationSymbols]))
 
+# All to string
+txtData['comment'] = txtData['comment'].apply(str)
+
+# Spelling corrections
+txtData['comment'] = [' '.join([spell(i) for i in x.split()]) for x in txtData['comment']]
+
+# Tokenization
+txtData['comment'] = txtData['comment'].apply(lambda x: word_tokenize(x))
+
+
 # Save to csv
-txtData.to_csv('preProcessedData.csv', sep='\t', encoding='utf-8', index=False, header=True)
+txtData.to_csv('processedData.csv', sep='\t', encoding='utf-8', index=False, header=True)
 
 # Read from csv
-# txtData = pd.read_csv('preProcessedData.csv', sep='\t', encoding='utf-8')
+newTxtData = pd.read_csv('processedData.csv', sep='\t', encoding='utf-8')
 
 
 # # NLTK Stopwords
@@ -63,20 +78,5 @@ txtData.to_csv('preProcessedData.csv', sep='\t', encoding='utf-8', index=False, 
 #     lambda x: ' '.join([word for word in x.split()
 #                         if word not in sidewayEmoticons]))
 # txtDataNltkNoSymbols['comment'] = txtDataNltkNoSymbols['comment'].apply(
-#     lambda x: ' '.join([word for word in x.split()
-#                         if word not in punctuationSymbols]))
-
-# # SKLearn Stopwords
-# txtDataSklearn = txtData.copy()
-# sklearnStopwords = text.ENGLISH_STOP_WORDS
-# txtDataSklearn['comment'] = txtDataSklearn['comment'].apply(
-#     lambda x: ' '.join([word for word in x.split()
-#                         if word not in sklearnStopwords]))
-
-# txtDataSklearnNoSymbols = txtDataSklearn.copy()  # Remove symbols
-# txtDataSklearnNoSymbols['comment'] = txtDataSklearnNoSymbols['comment'].apply(
-#     lambda x: ' '.join([word for word in x.split()
-#                         if word not in sidewayEmoticons]))
-# txtDataSklearnNoSymbols['comment'] = txtDataSklearnNoSymbols['comment'].apply(
 #     lambda x: ' '.join([word for word in x.split()
 #                         if word not in punctuationSymbols]))
