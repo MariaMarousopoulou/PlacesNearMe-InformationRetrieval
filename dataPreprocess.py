57e8b848d8bcb55843db43b52ccebd09c711e02b
MariaMarousopoulou <44573617+MariaMarousopoulou@users.noreply.github.com>
2021-12-04 13:05:18 +0200

    Preprocess corrections

--- dataPreprocess.py
+++ dataPreprocess.py
@@ -19,48 +19,49 @@ sidewayEmoticons = [':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)
                     ':c', ':{', '>:\\', ';(']
 
 # Load data
-txtData = pd.read_csv('dataset_ubicomp2013_tips.txt', sep='\t', encoding='ISO-8859-1', header=None)
-txtData.columns = ['storeId', 'comment']
+txtData = pd.read_csv('dataset_ubicomp2013_tips.txt', sep='\t', encoding='ISO-8859-1')
 
 # Save to csv before preprocess
-txtData.to_csv('rawData.csv', sep='\t', encoding='utf-8', index=False, header=True)
+txtData.to_csv('rawData.csv', sep=';', encoding='utf-8', index=False, header=True)
+
+txtData.columns = ['ID', 'Comment']
 
 # Missing values
 txtData.isnull().sum()  # No missing values
 
-txtData['comment'] = txtData['comment'].str.lower()  # convert to lowercase
+txtData['Comment'] = txtData['Comment'].str.lower()  # convert to lowercase
 
 # Remove extra whitespaces
-txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join(x.split()))
+txtData['Comment'] = txtData['Comment'].apply(lambda x: ' '.join(x.split()))
 
 # Remove AM/PM
-txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
+txtData['Comment'] = txtData['Comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
                                                                   ('(?:\d{2}|\d{1})(?:AM|PM|am|pm)')]))
 # Remove url
-txtData['comment'] = txtData['comment'].str.replace(r'http\S+', '')
+txtData['Comment'] = txtData['Comment'].str.replace(r'http\S+', '')
 
 # Remove emojis
-txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
+txtData['Comment'] = txtData['Comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
                                                                   sidewayEmoticons]))
 # Remove symbols
-txtData['comment'] = txtData['comment'].str.replace(r'[^\w\s]+', '')
+txtData['Comment'] = txtData['Comment'].str.replace(r'[^\w\s]+', '')
 
 # All to string
-txtData['comment'] = txtData['comment'].apply(str)
+txtData['Comment'] = txtData['Comment'].apply(str)
 
 # Spelling corrections
-txtData['comment'] = [' '.join([spell(i) for i in x.split()]) for x in txtData['comment']]
+txtData['Comment'] = [' '.join([spell(i) for i in x.split()]) for x in txtData['Comment']]
 
 # Porter Stemmer
 nltkPorterStemmer = PorterStemmer()
-txtData['comment'].apply(lambda x: [nltkPorterStemmer.stem(y) for y in x])
+txtData['Comment'].apply(lambda x: [nltkPorterStemmer.stem(y) for y in x])
 
 # NLTK Stopwords;
 nltkStopwords = stopwords.words('english')
-txtData['comment'] = txtData['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
+txtData['Comment'] = txtData['Comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in
                                                                   nltkStopwords]))
 # Save to csv
-txtData.to_csv('processedData.csv', sep='\t', encoding='utf-8', index=False, header=False)
+txtData.to_csv('processedData.csv', sep=';', encoding='utf-8', index=False, header=True)
 
 # Read from csv to check
-# newTxtData = pd.read_csv('processedData.csv', sep='\t', encoding='utf-8')
+# newTxtData = pd.read_csv('processedData.csv', sep=';', encoding='utf-8')
